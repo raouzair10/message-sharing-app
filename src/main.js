202495a40cbcbdba3deb00bef2d998d3742e8ab3
raouzair10 <muzair.bese21seecs@seecs.edu.pk>
2024-12-19 10:31:08 +0500

    Add style files

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import './assets/global.css'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(ElementPlus)
+  .mount('#app')
